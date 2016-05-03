0c1cf94c1997806e50b30c2e92ff33feb2352238
Kojin Oshiba <kojin.oshiba@gmail.com>
2016-05-03 00:33:28 -0400

    update file path

--- visualizer.py
+++ visualizer.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request, jsonify
 import networks, plotting
 
+import os
+from intro_to_flask import app
+
+port = int(os.environ.get("PORT", 5000))
+app.run(debug=True, host='0.0.0.0', port=port)
 
 app = Flask(__name__)
 
